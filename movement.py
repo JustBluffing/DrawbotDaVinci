4f99ed5ff47981ed6eea634c10f028b51ee3ede7
JustBluffing <71127055+JustBluffing@users.noreply.github.com>
2025-04-08 03:29:15 +0300

    Update movement.py

--- movement.py
+++ movement.py
@@ -1,3 +1,43 @@
 """
-Code for the physical movement of the robot.
+Code for the physical movement of the robot. Test.
 """
+from machine import Pin, Timer
+import utime
+ 
+dir_pin = Pin(16, Pin.OUT)
+step_pin = Pin(17, Pin.OUT)
+steps_per_revolution = 200
+ 
+# Initialize timer
+timer = Timer()
+ 
+def step(t):
+    global step_pin
+    step_pin.value(not step_pin.value())
+ 
+def rotate_motor(delay):
+    # Set up timer for stepping
+    timer.init(freq=1000000//delay, mode=Timer.PERIODIC, callback=step)
+ 
+def loop():
+    while True:
+        # Set motor direction clockwise
+        dir_pin.value(1)
+ 
+        # Spin motor slowly
+        rotate_motor(2000)
+        utime.sleep_ms(steps_per_revolution)
+        timer.deinit()  # stop the timer
+        utime.sleep(1)
+ 
+        # Set motor direction counterclockwise
+        dir_pin.value(0)
+ 
+        # Spin motor quickly
+        rotate_motor(1000)
+        utime.sleep_ms(steps_per_revolution)
+        timer.deinit()  # stop the timer
+        utime.sleep(1)
+ 
+if __name__ == '__main__':
+    loop()
